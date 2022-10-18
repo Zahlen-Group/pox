51402a017400cb865c82ac1717eb8ff6499887dd
Chris Douglas <chris_douglas@berkeley.edu>
2022-10-18 04:23:59 +0000

    Update dev container w/ advice from support

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 cd ext
-git clone https://github.com/Zahlen-Group/ei.git
+git clone -b groove https://github.com/Zahlen-Group/ei.git
 
 cd ei
 scripts/build_all.sh --install --with-tests --clean --parallel
