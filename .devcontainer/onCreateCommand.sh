1458f4834cfd3e4b22f655954ccd7813ea37360f
Chris Douglas <cdouglas@apache.org>
2022-10-19 10:13:32 -0700

    Move build to post-create step

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,7 +1,4 @@
-#/usr/bin/env bash
+#/usr/bin/env bash -x
 
 cd ext
 git clone -b groove https://github.com/Zahlen-Group/ei.git
-
-cd ei
-scripts/build_all.sh --install --with-tests --clean --parallel
---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash -x
+
+cd ei
+scripts/build_all.sh --install --with-tests --clean --parallel
